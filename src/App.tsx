dc981408eebfc3b2c37b30c572aa1f4073787141
stehfany <sthefanyclarismundo23@gmail.com>
2024-10-25 15:58:08 -0300

    base index1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
+import Base from './Base';
+import logo from './bebase.jpeg'; // Certifique-se de que o caminho está correto
+import './App.css'; // Importando estilos
 
 function App() {
+  const location = useLocation(); // Hook para obter a rota atual
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar com links */}
+      <nav className="navbar">
+        <Link to="/" className="App-link">Bebase</Link>
+        <span className="separator">|</span>
+        <Link to="/base" className="App-link">Quem somos</Link>
+      </nav>
+
+      {/* Renderiza o header e o conteúdo apenas na rota '/' */}
+      {location.pathname === '/' && (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Bem-vindo a!</p>
+          <h1 className="titulo">Bebase</h1>
+        </header>
+      )}
+
+      {/* Rotas definidas */}
+      <Routes>
+        <Route path="/base" element={<Base />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Base.tsx
@@ -0,0 +1,13 @@
+function Base() {
+    return (
+      <div>
+        <h1>Conheça um pouco mais sobre a Bebase</h1>
+        <p>Conteúdo da página Base.</p>
+       
+  
+      </div>
+    );
+  }
+  
+  export default Base;
+  
